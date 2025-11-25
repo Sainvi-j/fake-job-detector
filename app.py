fce1f08d5856f5423dcf45f225e591cd6ecd14eb
Sainvi-j <sainvijaiswal05@gmail.com>
2025-11-25 19:43:10 +0530

    chore: update app.py for starting up

--- app.py
+++ app.py
@@ -11,4 +11,6 @@ app.register_blueprint(main_bp)
 app.register_blueprint(admin_bp, url_prefix='/admin')
 
 if __name__ == '__main__':
+    print("Public App : http://127.0.0.1:5000")
+    print("Admin Login: http://127.0.0.1:5000/admin/login")
     app.run(debug=True)
